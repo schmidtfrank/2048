ebd077f3e1fb99e7bfc4d2c887a5c52f0b5f0b8c
Frank Schmidt <schmidtfrank190@gmail.com>
2026-03-04 23:06:27 -0600

    new tile and move right implemented

--- backend/game.py
+++ backend/game.py
@@ -31,8 +31,49 @@ class Game2048:
             else:
                 self.board[x][y] = 2
     
+    def new_tile(self):
+
+        valid_tiles = []
+        for y in range(3):
+            for x in range(3):
+                if self.board[y][x] == 0:
+                    valid_tiles.append((y,x))
+        
+        new_idx = random.randint(0,len(valid_tiles)-1)
+
+        self.board[valid_tiles[new_idx][0]][valid_tiles[new_idx][1]] = 2
     def move_right(self):
-        pass
+        #self.board[y][x] works like intuition
+        y = 0
+        while y <= 3:
+            x = 3
+            while x >= 0:
+                if self.board[y][x] == 0:
+                    #need to see if there is any number in this row to put in our position
+                    i = x-1
+                    while i >= 0:
+                        #found a match!
+                        if self.board[y][i] != 0:
+                            self.board[y][x] = self.board[y][i]
+                            self.board[y][i] = 0
+                            x += 1
+                            break
+                        i -= 1
+                elif self.board[y][x] != 0:
+                    #we need to check if something will merge into us here
+                    i = x-1
+                    while i >= 0:
+                        if self.board[y][i] == self.board[y][x]:
+                            self.board[y][x] *= 2
+                            self.score += self.board[y][x]
+                            self.board[y][i] = 0
+                            break
+                        i -= 1
+                x -= 1
+            y += 1
+        
+        self.new_tile()
+    
 
 
 GameInstance = Game2048()
